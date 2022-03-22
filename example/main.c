3eac7dd557af6a9f3354f65d11cba837502b43d5
IvanVnucec <vnucec.ivan@gmail.com>
2022-03-22 08:50:58 +0100

    add example

--- example/main.c
+++ example/main.c
@@ -1,8 +1,35 @@
-#include "main.h"
 #include "zs040/zs040.h"
+
+
+void user_uart_send_function(const uint8_t* const data, 
+    const unsigned len, 
+    ZS040_Status *status)
+{
+    // TODO
+
+    *status = ZS040_STATUS_SUCCESS;
+}
+
+
+void user_uart_receive_function(uint8_t* data, 
+    const unsigned len, 
+    unsigned timeout_ms, 
+    ZS040_Status *status)
+{
+    // TODO
+
+    *status = ZS040_STATUS_SUCCESS;
+}
 
 
 int main(void)
 {
+    ZS040_Status status;
+    ZS040_init(user_uart_send_function, user_uart_receive_function, &status);
+    
+    if (status == ZS040_STATUS_SUCCESS) {
+        ZS040_send("hello world", sizeof("hello world"), &status);
+    }
+    
     return 0;
 }
